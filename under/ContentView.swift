fc7323be9ed16ef2c96ca92de7eb69f95f01ae72
M.Ozgur Ozturk <ozturkozgurm@gmail.com>
2026-02-07 15:24:25 +0300

    Initial Commit

---
+++ under/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  under
+//
+//  Created by   Özgür Öztürk on 7.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ under/underApp.swift
@@ -0,0 +1,17 @@
+//
+//  underApp.swift
+//  under
+//
+//  Created by   Özgür Öztürk on 7.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct underApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
